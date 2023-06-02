0419a6a6e4560943fbf110c3e3e5d8ec5170bc40
twonezero <dnjsdudtm98@naver.com>
2023-06-02 14:40:43 +0900

    Review - Point transaction

--- camp-review/11-02-point-transaction/backend/src/apis/pointsTransactions/pointsTransactions.service.ts
+++ camp-review/11-02-point-transaction/backend/src/apis/pointsTransactions/pointsTransactions.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import {
   POINT_TRANSACTION_STATUS_ENUM,
   PointTransaction,
@@ -15,6 +15,7 @@ export class PointsTransactionsService {
     private readonly pointsTransactionsRepository: Repository<PointTransaction>,
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async create({
@@ -22,28 +23,56 @@ export class PointsTransactionsService {
     amount,
     user: _user,
   }: IPointsTransactionsServiceCreate): Promise<PointTransaction> {
-    // this.pointsTransactionsRepository.create(); // 등록을 위한 빈 객체 만들기
-    // this.pointsTransactionsRepository.insert(); // 결과는 못 받는 등록 방법
-    // this.pointsTransactionsRepository.update(); // 결과는 못 받는 수정 방법
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-    //1. point-transaction table 에 거래기록 저장
-    const pointTransaction = this.pointsTransactionsRepository.create({
-      impUid,
-      amount,
-      user: _user,
-      status: POINT_TRANSACTION_STATUS_ENUM.PAYMENT,
-    });
-    await this.pointsTransactionsRepository.save(pointTransaction);
+    try {
+      const pointTransaction = this.pointsTransactionsRepository.create({
+        impUid,
+        amount,
+        user: _user,
+        status: POINT_TRANSACTION_STATUS_ENUM.PAYMENT,
+      });
+      await queryRunner.manager.save(pointTransaction);
 
-    //2. user point 조회
-    const user = await this.usersRepository.findOneBy({ id: _user.id });
-    //3. user point 추가(update)
-    await this.usersRepository.update(
-      { id: _user.id },
-      { point: user.point + amount },
-    );
+      const user = await queryRunner.manager.findOneBy(User, { id: _user.id });
+
+      //객체를 생성해서 업데이트 될 내용을 담는다.
+      const updatedUser = this.usersRepository.create({
+        ...user,
+        point: user.point + amount,
+      });
+
+      await queryRunner.manager.save(updatedUser);
+
+      await queryRunner.commitTransaction();
+
+      return pointTransaction;
+    } catch (error) {
+      console.log(error);
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
+
+    // //1. point-transaction table 에 거래기록 저장
+    // const pointTransaction = this.pointsTransactionsRepository.create({
+    //   impUid,
+    //   amount,
+    //   user: _user,
+    //   status: POINT_TRANSACTION_STATUS_ENUM.PAYMENT,
+    // });
+    // await this.pointsTransactionsRepository.save(pointTransaction);
+
+    // //2. user point 조회
+    // const user = await this.usersRepository.findOneBy({ id: _user.id });
+    // //3. user point 추가(update)
+    // await this.usersRepository.update(
+    //   { id: _user.id },
+    //   { point: user.point + amount },
+    // );
 
     //4. 브라우저에 response
-    return pointTransaction;
   }
 }
